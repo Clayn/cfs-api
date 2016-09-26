b1ff9b590d85d0b90de0d82e73501b4124ab80c5
Clayn <clayn_osmato@gmx.de>
2016-09-26 11:54:48 +0200

    Feature: Introduced a new class (and subtype of SimpleFile) that only links to an file without actual being one. The Link class takes any SimpleFile and works as one. When accessing the link, the original file will be used. Added a tests for the new class

---
+++ src/main/java/net/bplaced/clayn/cfs/Link.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2016 Clayn <clayn_osmato@gmx.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bplaced.clayn.cfs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.Objects;
+import net.bplaced.clayn.cfs.ext.functional.TryingFunction;
+import net.bplaced.clayn.cfs.util.SizeUnit;
+
+/**
+ * A link represents a file without beeing a real file. A link works just like a
+ * real file but only delegates the calls to the linked file. This means reading
+ * and writing operations are made on the real file instead of the link file.
+ * Due to that behaviour links are 'followed' automatic.
+ *
+ * @author Clayn <clayn_osmato@gmx.de>
+ */
+public class Link implements SimpleFile
+{
+
+    private final SimpleFile linkedFile;
+
+    /**
+     * Creates a new link to the given file. One file can have more than one 
+     * link. 
+     * @param linkedFile the file to be linkend. Musn't be {@code null}.
+     * @since 0.3.0
+     */
+    public Link(SimpleFile linkedFile)
+    {
+        this.linkedFile = Objects.requireNonNull(linkedFile, "Can't create link for 'null'");
+    }
+
+    @Override
+    public boolean exists()
+    {
+        return linkedFile.exists();
+    }
+
+    @Override
+    public void create() throws IOException
+    {
+        linkedFile.create();
+    }
+
+    @Override
+    public void createSafe() throws IOException
+    {
+        linkedFile.createSafe();
+    }
+
+    @Override
+    public void delete() throws IOException
+    {
+        linkedFile.delete();
+    }
+
+    @Override
+    public IOStream open(OpeningType type) throws IOException
+    {
+        return linkedFile.open(type);
+    }
+
+    @Override
+    public InputStream openRead() throws IOException
+    {
+        return linkedFile.openRead();
+    }
+
+    @Override
+    public OutputStream openWrite() throws IOException
+    {
+        return linkedFile.openWrite();
+    }
+
+    @Override
+    public String getName()
+    {
+        return linkedFile.getName();
+    }
+
+    @Override
+    public long getSize() throws IOException
+    {
+        return linkedFile.getSize();
+    }
+
+    @Override
+    public double getSize(SizeUnit unit) throws IOException
+    {
+        return linkedFile.getSize(unit);
+    }
+
+    @Override
+    public <R> R as(TryingFunction<SimpleFile, R> formatter)
+    {
+        return linkedFile.as(formatter);
+    }
+
+    @Override
+    public Charset getCharset()
+    {
+        return linkedFile.getCharset();
+    }
+
+    @Override
+    public OutputStream openAppend() throws IOException
+    {
+        return linkedFile.openAppend();
+    }
+
+    @Override
+    public Directory getParent()
+    {
+        return linkedFile.getParent();
+    }
+
+    @Override
+    public String toString()
+    {
+        return linkedFile.toString();
+    }
+
+}
---
+++ src/test/java/net/bplaced/clayn/cfs/LinkTest.java
@@ -0,0 +1,294 @@
+/*
+ * Copyright (C) 2016 Clayn <clayn_osmato@gmx.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bplaced.clayn.cfs;
+
+import java.nio.charset.Charset;
+import net.bplaced.clayn.cfs.util.SizeUnit;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author Clayn <clayn_osmato@gmx.de>
+ */
+public class LinkTest
+{
+    
+    public LinkTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of exists method, of class Link.
+     */
+    @Test
+    public void testExists()
+    {
+        System.out.println("exists");
+        Link instance = null;
+        SimpleFile orig=getFile();
+        instance=new Link(orig);
+        
+        when(orig.exists()).thenReturn(false, true,false);
+        
+        assertFalse(instance.exists());
+        assertTrue(instance.exists());
+        assertFalse(instance.exists());
+        
+        verify(orig, times(3)).exists();
+    }
+
+    /**
+     * Test of create method, of class Link.
+     */
+    @Test
+    public void testCreate() throws Exception
+    {
+        System.out.println("create");
+        Link instance = null;
+        SimpleFile orig=getFile();
+        doNothing().when(orig).create();
+        instance=new Link(orig);
+        instance.create();
+        
+        verify(orig, times(1)).create();
+        
+    }
+
+    /**
+     * Test of createSafe method, of class Link.
+     */
+    @Test
+    public void testCreateSafe() throws Exception
+    {
+        System.out.println("createSafe");
+        Link instance = null;
+        SimpleFile orig=getFile();
+        doNothing().when(orig).createSafe();
+        instance=new Link(orig);
+        instance.createSafe();
+        
+        verify(orig, times(1)).createSafe();
+    }
+
+    /**
+     * Test of delete method, of class Link.
+     */
+    @Test
+    public void testDelete() throws Exception
+    {
+        System.out.println("delete");
+        Link instance = null;
+        SimpleFile orig=getFile();
+        instance=new Link(orig);
+        when(orig.exists()).thenReturn(true, false);
+        doNothing().when(orig).delete();
+        assertTrue(instance.exists());
+        instance.delete();
+        assertFalse(instance.exists());
+        
+        verify(orig, atLeast(1)).delete();
+    }
+    /**
+     * Test of openRead method, of class Link.
+     */
+    @Test
+    public void testOpenRead() throws Exception
+    {
+        System.out.println("openRead");
+        Link instance = null;
+        SimpleFile orig=getFile();
+        when(orig.openRead()).thenReturn(null);
+        instance=new Link(orig);
+        instance.openRead();
+        
+        verify(orig, times(1)).openRead();
+    }
+
+    /**
+     * Test of openWrite method, of class Link.
+     */
+    @Test
+    public void testOpenWrite() throws Exception
+    {
+        System.out.println("openWrite");
+        Link instance = null;
+        SimpleFile orig=getFile();
+        when(orig.openWrite()).thenReturn(null);
+        instance=new Link(orig);
+        instance.openWrite();
+        
+        verify(orig, times(1)).openWrite();
+    }
+
+    /**
+     * Test of getName method, of class Link.
+     */
+    @Test
+    public void testGetName()
+    {
+        System.out.println("getName");
+        Link instance = null;
+        String expResult = "Test.txt";
+        
+        SimpleFile orig=getFile();
+        instance=new Link(orig);
+        when(orig.getName()).thenReturn(expResult);
+        assertEquals(expResult, instance.getName());
+        verify(orig, atLeast(1)).getName();
+    }
+
+    private SimpleFile getFile()
+    {
+        return mock(SimpleFile.class);
+    }
+    
+    /**
+     * Test of getSize method, of class Link.
+     */
+    @Test
+    public void testGetSize_0args() throws Exception
+    {
+        System.out.println("getSize");
+        Link instance;
+        SimpleFile orig=mock(SimpleFile.class);
+        instance=new Link(orig);
+        long [] results=new long[]{1l,2l,3l,4l,5l};
+        when(orig.getSize()).thenReturn(1l, 2l,3l,4l,5l);
+        for(int i=0;i<results.length;++i)
+        {
+            assertEquals(results[i], instance.getSize());
+        }
+        verify(orig, atLeast(results.length)).getSize();
+    }
+
+    /**
+     * Test of getSize method, of class Link.
+     */
+    @Test
+    public void testGetSize_SizeUnit() throws Exception
+    {
+        System.out.println("getSize");
+        Link instance = null;
+        SimpleFile orig=mock(SimpleFile.class);
+        when(orig.getSize(SizeUnit.BYTE)).thenReturn(1000.0);
+        when(orig.getSize(SizeUnit.KILO_BYTE)).thenReturn(1.0);
+        instance=new Link(orig);
+        
+        double expected=1000.0;
+        double result=instance.getSize(SizeUnit.BYTE);
+        assertEquals(expected, result, 0.01);
+        expected=1.0;
+        result=instance.getSize(SizeUnit.KILO_BYTE);
+        assertEquals(expected, result, 0.01);
+        verify(orig,atLeast(2)).getSize(any());
+    }
+
+    /**
+     * Test of getCharset method, of class Link.
+     */
+    @Test
+    public void testGetCharset()
+    {
+        System.out.println("getCharset");
+        Link instance = null;
+        SimpleFile orig=mock(SimpleFile.class);
+        instance=new Link(orig);
+        when(orig.getCharset()).thenReturn(Charset.defaultCharset());
+        
+        assertEquals(Charset.defaultCharset(), instance.getCharset());
+        verify(orig,atLeast(1)).getCharset();
+        
+    }
+
+    /**
+     * Test of getParent method, of class Link.
+     */
+    @Test
+    public void testGetParent()
+    {
+        System.out.println("getParent");
+        Link instance = null;
+        Directory expResult = mock(Directory.class);
+        SimpleFile orig=mock(SimpleFile.class);
+        when(orig.getParent()).thenReturn(expResult);
+        String dirName="Dir";
+        when(expResult.getName()).thenReturn(dirName);
+        
+        instance=new Link(orig);
+        
+        Directory parent=instance.getParent();
+        assertNotNull(parent);
+        assertEquals(dirName, parent.getName());
+        
+        verify(orig,atLeast(1)).getParent();
+        verify(expResult,atLeast(1)).getName();
+    }
+
+    /**
+     * Test of toString method, of class Link.
+     */
+    @Test
+    public void testToString()
+    {
+        System.out.println("toString");
+        Link instance = null;
+        SimpleFile orig=mock(SimpleFile.class);
+        String name="FileName.txt";
+        stub(orig.toString()).toReturn(name);
+        instance=new Link(orig);
+        
+        assertEquals(name, instance.toString());
+        
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testNew()
+    {
+        System.out.println("new");
+        Link instance=null;
+        SimpleFile orig=mock(SimpleFile.class);
+        instance=new Link(orig);
+        Link link = new Link(null);
+    }
+    
+}
