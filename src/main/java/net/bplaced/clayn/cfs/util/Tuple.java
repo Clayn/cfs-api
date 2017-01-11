a6bb87d6e9f1ad5a9530bc2f7a28086302242515
Clayn <clayn_osmato@gmx.de>
2017-01-11 09:06:50 +0100

    Added a tuple generation method to be more flexible. Updated some documentation

--- src/main/java/net/bplaced/clayn/cfs/util/Tuple.java
+++ src/main/java/net/bplaced/clayn/cfs/util/Tuple.java
@@ -16,12 +16,16 @@
  */
 package net.bplaced.clayn.cfs.util;
 
+import java.util.function.Supplier;
+
 /**
- * Utility class that holds to values of the same or different types.
+ * Utility class that holds to values of the same or different types. This
+ * implementation is used to not include any dependencie for a tuple.
  *
  * @author Clayn <clayn_osmato@gmx.de>
  * @param <T> the type for the first value
  * @param <U> the type for the second value
+ * @since 0.1.0
  */
 public final class Tuple<T, U>
 {
@@ -31,6 +35,8 @@ public final class Tuple<T, U>
 
     /**
      * Creates a new empty tuple
+     *
+     * @since 0.1.0
      */
     public Tuple()
     {
@@ -41,6 +47,7 @@ public final class Tuple<T, U>
      *
      * @param first the first value to store
      * @param second the second value to store
+     * @since 0.1.0
      */
     public Tuple(T first, U second)
     {
@@ -48,11 +55,23 @@ public final class Tuple<T, U>
         this.second = second;
     }
 
+    /**
+     * Sets the first value of this tuple.
+     *
+     * @param first the new first value
+     * @since 0.1.0
+     */
     public void setFirst(T first)
     {
         this.first = first;
     }
 
+    /**
+     * Sets the second value of this tuple.
+     *
+     * @param second the new second value
+     * @since 0.1.0
+     */
     public void setSecond(U second)
     {
         this.second = second;
@@ -62,6 +81,7 @@ public final class Tuple<T, U>
      * Returns the first value. May be {@code null}
      *
      * @return the first value
+     * @since 0.1.0
      */
     public T getFirst()
     {
@@ -72,6 +92,7 @@ public final class Tuple<T, U>
      * Returns the second value. May be {@code null}
      *
      * @return the second value
+     * @since 0.1.0
      */
     public U getSecond()
     {
@@ -83,6 +104,21 @@ public final class Tuple<T, U>
     {
         return "Tuple{" + "first=" + first + ", second=" + second + '}';
     }
-    
-   
+
+    /**
+     * Creates a new tuple using the values provided by the given suppliers. 
+     * This is to convert other tuple or tuple like implementations into a 
+     * tuple expected by the cfs. 
+     * @param <T> the type of the first value
+     * @param <U> the type of the second value
+     * @param firstSup the supplier that provides the first value
+     * @param secondSub the supplier that provides the second value
+     * @return a new tuple that with the values provided by the suppliers
+     * @since 0.3.0
+     */
+    public static <T, U> Tuple<T, U> create(Supplier<? extends T> firstSup,
+            Supplier<? extends U> secondSub)
+    {
+        return new Tuple<>(firstSup.get(), secondSub.get());
+    }
 }
